843ab04f5751fe0f43259e52cd72c7004e9f7d34
Mohsin Alam <43788631+mohsin250999@users.noreply.github.com>
2022-09-27 23:39:03 +0530

    Time: 60 ms (97.52%), Space: 15.1 MB (50.61%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,10 @@
 class Solution:
    def twoSum(self, nums: List[int], target: int) -> List[int]:
-       seen = {}
-       for i, value in enumerate(nums): #1
-           remaining = target - nums[i] #2
-           
-           if remaining in seen: #3
-               return [i, seen[remaining]]  #4
-           else:
-               seen[value] = i  #5
+    hashmap={}
+    for i,n in enumerate(nums):
+        diff=target-n
+        if diff in hashmap:
+            return [hashmap[diff],i]
+        hashmap[n]=i
+        
+                
